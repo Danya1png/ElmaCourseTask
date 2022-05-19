3c1b16d5ddbc298eba8faf3cdb3110e81370942b
Danya-png <vaserman267@gmail.com>
2022-05-20 00:36:45 +0400

    Changed counter init value

--- ElmaCourseTask_FizzBuzz/ElmaCourseTask_FizzBuzz.cpp
+++ ElmaCourseTask_FizzBuzz/ElmaCourseTask_FizzBuzz.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     const int MAX_COUNTER_VALUE = 100;
-    for (int counter = 0; counter <= MAX_COUNTER_VALUE; counter++)
+    for (int counter = 1; counter <= MAX_COUNTER_VALUE; counter++)
     {
         if (counter % 3 == 0) {
             cout << "Fizz";
